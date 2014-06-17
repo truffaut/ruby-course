a41208d552ce73d06712b794ed92ce04117076af
DJ Daniels <dj.daniels90@gmail.com>
2014-06-17 16:02:16 -0700

    made formating much more clean and completed everything

--- lib/task-manager.rb
+++ lib/task-manager.rb
@@ -13,7 +13,7 @@ module TM
   end
 
   def self.parse_command
-    print "$: "
+    print "TM-$: "
     # partition only partitions up to the first instance
     input = gets.chomp
     @history << input
@@ -21,28 +21,41 @@ module TM
     command = input.first
     # split the remaining argument up
     args = input.last.split(" ")
-    puts args
-    puts args.count
+
     case command
       when "mark"
+        # marks a task as completed
+        # 1-takes 1 argument: tid
         TM.cmd_mark(args)
       when 'add'
+        # adds a task to a project
+        # 3-takes 3 arguments: pid, priority, desc
         TM.cmd_add(args)
-      when 'history'
-        TM.cmd_history(args)
-      when 'show'
-        TM.cmd_show(args)
+      when 'history', 'show'
+        # show the completed tasks
+        # 1-takes one argument: project_id
+        if command == "history"
+          TM.history_show_helper(args, :history)
+        elsif command == "show"
+          TM.history_show_helper(args, :show)
+        end
       when 'create'
+        # create a project
+        # 1-takes one argument: project_name
         TM.cmd_create(args)
       when 'list'
+        # Lists all projects
+        # 0-takes no arguments
         TM.cmd_list()
       when 'help'
+        # shows a list of all commands
+        # 0-takes no arguments
         TM.help
       when 'exit', 'q', 'quit'
         puts "Thank you for using the PM Terminal"
         @active = false
       else
-        puts "I'm sorry hal, I am unable to do that..."
+        puts "I'm sorry, I am unable to do that..."
     end
   end
 #
@@ -50,20 +63,17 @@ module TM
 #
   def self.cmd_mark(args)
     if args.count != 1
-      TM::PMTerminal.invalid_args("arg count")
-      return nil
-    elsif args.first.is_a Integer
-      TM::PMTerminal.invalid_args("need integers")
+      TM.invalid_args("arg count")
       return nil
     end
-    tid = args.pop
-    TM::Task.master_task_list[tid].mark_complete
+    tid = Integer(args.first)
+    TM::Task.get_task(tid).mark_complete
   end
 
   def self.cmd_add(args)
 
     if args.count < 3
-      TM::PMTerminal.invalid_args("arg count")
+      TM.invalid_args("arg count")
       return nil
     end
 
@@ -71,7 +81,7 @@ module TM
       project_id = Integer(args[0])
       priority = Integer(args[1])
     rescue
-      TM::PMTerminal.invalid_args("need integers")
+      TM.invalid_args("need integers")
       return nil
     end
 
@@ -86,46 +96,54 @@ module TM
     puts "-- DESCRIPTION: #{desc}"
   end
 
-  def self.cmd_history(args)
-    # TODO:
-    # Takes one argument
-    # PID
-    # show completed tasks for project id
+
+  def self.history_show_helper(args, type)
+    # type is either :show or :history
+    # completed will execute the show
     if args.count != 1
       puts "Wrong number of arguments."
       return nil
+    end
+
+    pid = Integer(args[0])
+    project = TM::Project.get_project(pid)
+
+    if project == nil
+      puts "project with #{pid} could not be found"
+      return nil
+    end
+
+    puts "ID: #{project.pid} - NAME: #{project.name}"
+
+    if type == :history
+      self.cmd_history(project)
+    elsif type == :show
+      self.cmd_show(project)
     end
 
   end
 
-  def self.cmd_show(args)
-    # TODO
+  def self.cmd_history(project)
+    # TODO:
+    # Takes one argument
+    # PID
+    # show completed tasks for project id
+    project.completed_tasks.each do |tk|
+      self.print_task_info({:priority => tk.priority,
+        :pid => tk.pid, :desc => tk.desc,
+        :ctime => tk.creation_time, :tid => tk.task_id})
+    end
+  end
+
+  def self.cmd_show(project)
     # Takes one argument
     # PID
     # show the remaining tasks for project id
-    if args.count != 1
-      puts "Wrong number of arguments."
-      return nil
-    end
-    puts "ARGUMENTS NUMBER: #{args.count}"
-    puts args
-    pid = Integer(args[0])
-    project = TM::Project.get_project(pid)
-
     project.incomplete_tasks.each do |tk|
-      priority = tk.priority
-      pid = tk.pid
-      desc = tk.desc
-      tim = tk.creation_time
-      tid = tk.task_id
-
-      puts
-      "Priority -- Task ID -- Creation Time
-         #{priority} -- #{tid}  -- #{tim}
-          Description: #{desc}"
+      self.print_task_info({:priority => tk.priority,
+        :pid => tk.pid, :desc => tk.desc,
+        :ctime => tk.creation_time, :tid => tk.task_id})
     end
-
-
   end
 
   def self.cmd_create(args)
@@ -155,7 +173,13 @@ module TM
 # Begin Status Messages
 #
   def self.invalid_args(type)
-    puts "invalid number of arguments! #{type}"
+    case type
+    when :number
+
+    when :type
+      puts "invalid number of arguments! #{type}"
+    end
+
   end
 
   def self.start_message
@@ -165,7 +189,8 @@ module TM
   end
 
   def self.help
-    puts "Available Commands:
+  puts "
+  Available Commands:
   -- <name>-> <----<args>------> <-------<info>------------------------->
   -- help                       - Show these commands again
   -- list                       - List all projects
@@ -174,7 +199,18 @@ module TM
   -- history  PID               - Show completed tasks for project with id=PID
   -- add      PID PRIORITY DESC - Add a new task to project with id=PID
   -- mark     TID               - Mark task with id=TID as complete
-  -- exit/q,quit                       - exits the program"
+  -- exit/q,quit                       - exits the program
+  "
+
+  end
+
+  # :priority, :desc, :pid, :ctime, :tid
+  def self.print_task_info(tsk)
+    puts "
+    Task ID: #{tsk[:tid]}
+    Priority: #{tsk[:priority]} | Creation Time: #{tsk[:ctime]}
+    #{tsk[:desc]}
+    "
   end
 #
 # End Status Messages
@@ -185,5 +221,5 @@ end
 require_relative 'task-manager/task.rb'
 require_relative 'task-manager/project.rb'
 
-# TM::run
+TM::run
 
