129dcf8153e1eb3be831800897af807a8ebb8e2f
DJ Daniels <dj.daniels90@gmail.com>
2014-06-18 15:17:14 -0700

    added method to reset the singleton class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 # Require our project, which in turns requires everything else
 require './lib/task-manager.rb'
 require 'rspec/mocks'
+
+RSpec.configure do |config|
+  # Configure each test to always use a new singleton instance
+  config.before(:each) do
+    DBI.instance_variable_set(:@__db_instance, nil)
+  end
+end
