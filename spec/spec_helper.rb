3ca4d62d67806da80c9ae5bdc5fa107e8d9f8d6a
DJ Daniels <dj.daniels90@gmail.com>
2014-06-18 17:23:26 -0700

    changed the namespace for the helper function to work correctly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,6 @@ require 'rspec/mocks'
 RSpec.configure do |config|
   # Configure each test to always use a new singleton instance
   config.before(:each) do
-    DBI.instance_variable_set(:@__db_instance, nil)
+    TM.instance_variable_set(:@__db_instance, nil)
   end
 end
