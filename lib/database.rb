be8cc8dd5b48026223dee092c55b4c9d701ed8db
DJ Daniels <dj.daniels90@gmail.com>
2014-06-18 17:22:49 -0700

    created data model scheme, join tables, user table, NEEDS TESTING

--- lib/database.rb
+++ lib/database.rb
@@ -12,18 +12,6 @@ module TM
       @db = PG.connect(host: @HOST, dbname: @DATABASE_NAME)
     end
 
-    def store_project()
-
-    end
-
-    def store_task()
-
-    end
-
-    def store_user()
-
-    end
-
     def list_projects
       command = <<-SQL
         SELECT * FROM projects;
@@ -38,20 +26,33 @@ module TM
         VALUES('#{name}');
       SQL
       @db.exec(command)
+      proj = @db.exec("SELECT * FROM projects ORDER BY id DESC LIMIT 1").values.flatten
+      # returns a single project entity that was just created
+      TM::Project.new(proj[0], proj[1])
     end
 
-    #
-    # Database Re-int Functions
-    #
+    def get_project(pid)
+      command = <<-SQL
+        SELECT * FROM projects WHERE id='#{pid}'
+      SQL
+      results = @db.exec(command).values.flatten
+      # returns a single project entity specified by ID
+      TM::Project.new(results[0], results[1])
+    end
+
+###########################################################################
+######################## Database Reinitialization ########################
+###########################################################################
     def create_all_tables
       create_projects_table
       create_tasks_table
       create_users_table
+      # create_usersprojects_table
+      # create_projectstasks_table
     end
 
     def clear_db(check)
       return nil if !check
-      # TM::db.exec("DROP schema public cascade;")
       @db.exec("DROP schema public cascade;")
       @db.exec("CREATE schema public;")
       puts "Database Cleared"
@@ -64,42 +65,74 @@ module TM
       puts "Reinitialization Complete"
     end
 
-    #
-    # Database Schema
-    #
-
+###########################################################################
+############################# Database Schema #############################
+###########################################################################
     def create_projects_table
-      proj_table_schema = <<-SQL
+      command = <<-SQL
         CREATE TABLE projects(
         name text,
         id SERIAL,
         PRIMARY KEY(id)
         );
       SQL
-      @db.exec(proj_table_schema)
+      @db.exec(command)
       puts "Projects Table Created"
     end
 
     def create_tasks_table
-      tasks_table_schema = <<-SQL
+      command = <<-SQL
         CREATE TABLE tasks(
         id SERIAL,
         priority integer,
         creationTime text,
         description text,
         completed boolean,
-        proj_id integer REFERENCES projects(id),
         PRIMARY KEY(id)
         );
       SQL
-      @db.exec(tasks_table_schema)
+      @db.exec(command)
       puts "Tasks Table Created"
     end
 
     def create_users_table
-      # TODO:
+      command = <<-SQL
+        CREATE TABLE users(
+        name text,
+        id SERIAL,
+        PRIMARY KEY(id)
+        );
+      SQL
+      @db.exec(command)
       puts "Users Table Created"
     end
+    # JOIN TABLES
+    def create_usersprojects_table
+      command = <<-SQL
+        CREATE TABLE users_projects(
+        id SERIAL,
+        PRIMARY KEY(id)
+        proj_id integer REFERENCES projects(id),
+        user_id integer REFERENCES users(id)
+        );
+      SQL
+      @db.exec(command)
+      puts "Users-Projects Table Created"
+    end
+
+    def create_projectstasks_table
+      command = <<-SQL
+        CREATE TABLE projects_tasks(
+        id SERIAL,
+        PRIMARY KEY(id)
+        proj_id integer REFERENCES projects(id),
+        task_id integer REFERENCES task(id),
+        );
+      SQL
+      @db.exec(command)
+      puts "Users Table Created"
+    end
+
 
   end
 
