306aeff1e2549b3b1a7f2569fec8403032f29c2f
DJ Daniels <dj.daniels90@gmail.com>
2014-06-21 16:14:06 -0700

    changed the owned by to owned_by_eid

--- lib/task-manager/task.rb
+++ lib/task-manager/task.rb
@@ -2,18 +2,18 @@ require_relative "../orm.rb"
 
 class TM::Task
 
-  attr_reader :pid, :desc, :priority, :creation_time, :complete, :tid
+  attr_reader :pid, :desc, :priority, :creation_time, :complete, :tid, :owned_by_eid
 
   def initialize(params)
-    # tid,      priority,   desc,   creation_time, pid,     complete)
-    # params[1], params[1], params[2], params[3], params[4], params[5]
+    # tid,      priority,   desc,   creation_time, pid,     complete     owned_by_id)
+    # params[1], params[1], params[2], params[3], params[4], params[5], params[6]
     @tid = Integer(params[0])
     @priority = Integer(params[1])
     @desc = params[2]
     @creation_time = Time.parse(params[3])
     @pid = Integer(params[4])
     params[5] == "f" ? @complete = false : @complete = true
-    @owned_buy = params[6]
+    @owned_by_eid = params[6]
   end
 
   # TODO: Implement the mark_completed method within the class
