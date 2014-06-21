087d460ebeeb1567fa483d23cd90bf10a828ae50
DJ Daniels <dj.daniels90@gmail.com>
2014-06-21 15:40:40 -0700

    created another supplemental method to add more task related relations to the database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,8 +13,16 @@ end
 
 # This is for internal testing purposes as well as rspec tests
 def populate_projects_and_employees
+  # employee 1 - 3 projects = pid1,pid2,pid3
+  # employee 2 - 2 projects = pid3,pid4, pid5
   5.times { |i| TM::orm.project_create("Project #{i+1}")}
   2.times { |i| TM::orm.employee_create("Employee #{i+1}")}
-  3.times { |i| TM::orm.project_recruit(i+1, 1)}
-  2.times { |i| TM::orm.project_recruit(i+3, 2)}
+end
+
+def populate_tasks_projects_employees
+  populate_projects_and_employees
+  3.times {|i| TM::orm.task_create(1, i*2, "task #{i+1}")}
+  # pid1 = 3 tasks, tid1,tid2,tid3
+  5.times {|i| TM::orm.task_create(4, i*2, "task #{i+1}")}
+  # pid4 = 5 tasks. tid4,5,6,7,8
 end
