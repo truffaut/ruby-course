f322d0b44b78a114f99a1ed423374f0c27b1f192
DJ Daniels <dj.daniels90@gmail.com>
2014-06-21 14:53:33 -0700

    created a helper method to populate the database with a small relational structure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,11 @@ def are_tasks_equal(t1, t2)
     t1.desc == t2.desc &&
     t1.tid == t2.tid
 end
+
+# This is for internal testing purposes as well as rspec tests
+def populate_projects_and_employees
+  5.times { |i| TM::orm.project_create("Project #{i+1}")}
+  2.times { |i| TM::orm.employee_create("Employee #{i+1}")}
+  3.times { |i| TM::orm.project_recruit(i+1, 1)}
+  2.times { |i| TM::orm.project_recruit(i+3, 2)}
+end
