e0648f3a2c3e5f694b712932f519402e34a42c1e
Mike Ornellas <mike@makersquare.com>
2014-07-14 13:49:25 -0700

    solution

--- lib/honkr/scripts/sign_in.rb
+++ lib/honkr/scripts/sign_in.rb
@@ -2,7 +2,19 @@ module Honkr
   class SignIn
 
     def self.run(params)
-      # TODO
+      user = Honkr.db.get_user_by_username(params[:username])
+      if user.nil?
+        return { :success? => false, :error => :user_does_not_exist }
+      end
+
+      pass = user.has_password?(params.delete(:password))
+      if !pass
+        return { :success? => false, :error => :invalid_password}
+      end
+
+      session_id = Honkr.db.create_session({:user_id => user.id})
+
+      return { :success? => true, :session_id => session_id}
     end
 
   end
